21e3982b0669050151dbcc3ce6c3115b1b53030f
lelong1304 <lelong1304@gmail.com>
2023-03-18 21:01:59 +0100

    first commit: create interface Match and class Player

---
+++ src/main/java/org/kata/model/Match.java
@@ -0,0 +1,5 @@
+package org.kata.model;
+
+public interface Match {
+    void play(Player player1, Player player2);
+}
---
+++ src/main/java/org/kata/model/Player.java
@@ -0,0 +1,10 @@
+package org.kata.model;
+
+import lombok.Data;
+
+@Data
+public class Player {
+    private String name;
+    private int point;
+
+}
