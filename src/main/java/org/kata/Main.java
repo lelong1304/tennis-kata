76bbd6a8d35a1fc450112496711d285529a90882
lelong1304 <lelong1304@gmail.com>
2023-03-20 01:54:29 +0100

    Main : add game example

--- src/main/java/org/kata/Main.java
+++ src/main/java/org/kata/Main.java
@@ -1,7 +1,22 @@
 package org.kata;
 
+import org.kata.model.Match;
+import org.kata.model.Player;
+import org.kata.model.TennisMatch;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // ex : game 1
+        String game1Input = "ABABAA";
+        Match tennisMatch = new TennisMatch(game1Input);
+        tennisMatch.play();
+
+        // ex : game 2
+        String game2Input = "ABABABBAABBB";
+        Player player1 = new Player("Long LE", 'A');
+        Player player2 = new Player("Arnaud Mary", 'B');
+        Match tennisMatch2 = new TennisMatch(game2Input, player1, player2);
+        tennisMatch2.play();
+
     }
 }
